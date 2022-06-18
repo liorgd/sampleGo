dbb6063eee2cbc199db390c946724b15d022427a
Lior_Gd <Lior_Gd@Dell.com>
2022-06-18 18:37:07 +0300

    refactor

---
+++ rest/rest.go
@@ -0,0 +1,53 @@
+package rest
+
+import (
+	"github.com/gin-gonic/gin"
+	"main/db"
+	"net/http"
+)
+
+// represents data about a record.
+type Record struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Content string  `json:"content"`
+	Views  int32 `json:"views"`
+	Timestamp int32 `json:"timestamp"`
+}
+
+var records = []Record{
+	{ID: "1", Title: "Blue Train", Content: "John Coltrane", Views: 56, Timestamp: 3452341},
+	{ID: "2", Title: "Jeru", Content: "John Coltrane", Views: 57, Timestamp: 3452342},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Content: "John Coltrane", Views: 58, Timestamp: 3452343},
+}
+
+func GetRecords(c *gin.Context) {
+	db.GetRecords();
+	c.IndentedJSON(http.StatusOK, records)
+}
+
+func PostRecord(c *gin.Context) {
+	var newRecord Record
+
+	if err := c.BindJSON(&newRecord); err != nil {
+		return
+	}
+
+	// Add the new record to the slice.
+	records = append(records, newRecord)
+	c.IndentedJSON(http.StatusCreated, newRecord)
+}
+
+func GetRecordByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range records {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "record not found"})
+}
