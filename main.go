c7d7ea55c5f644f0dbcbe43797c06152b139c0a5
liorgd <liorgd@gmail.com>
2022-06-18 12:24:01 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-// album represents data about a record album.
-type album struct {
+// represents data about a record.
+type record struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Content string  `json:"content"`
@@ -14,39 +14,39 @@ type album struct {
 	Timestamp int32 `json:"timestamp"`
 }
 
-var albums = []album{
+var records = []record{
 	{ID: "1", Title: "Blue Train", Content: "John Coltrane", Views: 56, Timestamp: 3452341},
 	{ID: "2", Title: "Jeru", Content: "John Coltrane", Views: 57, Timestamp: 3452342},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Content: "John Coltrane", Views: 58, Timestamp: 3452343},
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getRecords(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, records)
 }
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newRecord record
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newRecord); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	// Add the new record to the slice.
+	records = append(records, newRecord)
+	c.IndentedJSON(http.StatusCreated, newRecord)
 }
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
+// getAlbumByID locates the record whose ID value matches the id
+// parameter sent by the client, then returns that record as a response.
+func getRecordByID(c *gin.Context) {
 	id := c.Param("id")
 
 	// Loop over the list of albums, looking for
 	// an album whose ID value matches the parameter.
-	for _, a := range albums {
+	for _, a := range records {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
@@ -57,9 +57,9 @@ func getAlbumByID(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
+	router.GET("/records", getRecords)
+	router.GET("/records/:id", getRecordByID)
+	router.POST("/records", postAlbums)
 
 	router.Run("localhost:3000")
 }
